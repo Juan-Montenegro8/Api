a74e3eee764b9d634f364c3eca5f544dca629a26
Juan-Montenegro8 <jj.monte2002@gmail.com>
2024-05-07 17:15:51 -0500

    second commit

---
+++ src/main/java/com/example/api/model/entity/cliente.java
@@ -0,0 +1,16 @@
+package com.example.api.model.entity;
+
+import java.util.*;;
+
+public class cliente {
+    
+    private Integer idCliente;
+
+    private String nombre;
+
+    private String apellido;
+
+    private String correo;
+
+    private Date fechaRegistro;
+}
