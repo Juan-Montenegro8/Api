67aa90e1a044ddd4d3765fae35a1c6c89c1a26f9
Juan-Montenegro8 <jj.monte2002@gmail.com>
2024-05-08 16:34:33 -0500

    architecture commit

--- src/main/java/com/example/api/model/entity/cliente.java
+++ src/main/java/com/example/api/model/entity/cliente.java
@@ -2,16 +2,8 @@ package com.example.api.model.entity;
 
 import java.io.Serializable;
 import java.util.*;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import lombok.ToString;
+import jakarta.persistence.*;
+import lombok.*;
 
 @Data
 @AllArgsConstructor
