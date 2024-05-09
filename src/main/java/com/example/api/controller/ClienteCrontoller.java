f466ec21adcfb01110798b34574f40c5c9e8a149
Juan-Montenegro8 <jj.monte2002@gmail.com>
2024-05-09 11:51:49 -0500

    second commit controller

--- src/main/java/com/example/api/controller/ClienteCrontoller.java
+++ src/main/java/com/example/api/controller/ClienteCrontoller.java
@@ -6,7 +6,9 @@ import com.example.api.model.entity.cliente;
 import com.example.api.service.ICliente;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -23,22 +25,26 @@ public class ClienteCrontoller {
     private ICliente clienteservice;
 
     @PostMapping("cliente")
+    @ResponseStatus(HttpStatus.CREATED)
     public cliente create(@RequestBody cliente Cliente){
         return clienteservice.save(Cliente);
     }
     
     @PutMapping("cliente")
+    @ResponseStatus(HttpStatus.CREATED)
     public cliente update(@RequestBody cliente Cliente){
         return clienteservice.save(Cliente);
     }
 
     @DeleteMapping("cliente/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
     public void delete(@PathVariable Integer id){
         cliente clientedelete = clienteservice.findById(id);
         clienteservice.delete(clientedelete);
     }
 
     @GetMapping("cliente/{id}")
+    @ResponseStatus(HttpStatus.OK)
     public cliente showById(@PathVariable Integer id){
         return clienteservice.findById(id);
     }
